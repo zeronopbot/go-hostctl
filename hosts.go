9bab2b257321906969466d06787162a2e6c1d0d1
zeronopbot <znopbot@alphaseclabs.com>
2021-06-21 19:36:45 -0400

    Adding host parsing, tests and fixes

--- hosts.go
+++ hosts.go
@@ -5,25 +5,129 @@ import (
 	"bytes"
 	"fmt"
 	"io"
-	"log"
+	"net"
 	"os"
+	"strings"
 )
 
 type HostEntry struct {
 	rawLine []byte
 	Comment string
-	IPAddress string
-	Hostname string
+	IPAddress net.IP
+	DomainName string
 	Alias string
+}
+
+func (he *HostEntry) Validate() error {
+
+	// Just a comment line
+	if he.IPAddress == nil && len(he.DomainName) <= 0 && len(he.Alias) <= 0 && len(he.Comment) > 0 {
+		return nil
+	}
+
+	// Non comment line should have valid IP
+	if he.IPAddress == nil {
+		return fmt.Errorf("no valid ip address parsed")
+	}
+
+	// Non comment line should have at least a domain name
+	if len(he.DomainName) <= 0 {
+		return fmt.Errorf("entry must have a domain name if ip address is set")
+	}
+
+	return nil
+}
+
+func (he HostEntry) Write(writer io.Writer, prefix string) error {
+
+	if he.IPAddress == nil {
+
+		line := fmt.Sprintf("%s%s\r\n", prefix, he.Comment)
+		if _, err := writer.Write([]byte(line)); err != nil {
+			return err
+		}
+		return nil
+	}
+
+	line := he.IPAddress.String()
+	for _, str := range []string{he.DomainName, he.Alias, he.Comment} {
+
+		if len(str) <= 0 {
+			continue
+		}
+
+		line += fmt.Sprintf("\t%s", str)
+	}
+
+	line = fmt.Sprintf("%s\r\n", line)
+	if _, err := writer.Write([]byte(line)); err != nil {
+		return err
+	}
+	return nil
+}
+
+func NewHostEntry(line []byte) (*HostEntry, error) {
+
+	if line == nil || len(line) <= 0 {
+		return nil, fmt.Errorf("invalid line, empty or nil")
+	}
+
+	tokens, err := tokenize(line)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(tokens) == 0 {
+		return nil, fmt.Errorf("no tokens parsed for line: '%s'", line)
+	}
+
+	hostEntry := &HostEntry{
+		rawLine:    line,
+	}
+
+	for n, token := range tokens {
+
+		tok := token
+
+		// Comment is last or only thing in entry
+		if strings.HasPrefix(token, "#") {
+			hostEntry.Comment = tok
+			return hostEntry, nil
+		}
+
+		switch n {
+
+		// ip address
+		case 0:
+			hostEntry.IPAddress = net.ParseIP(tok)
+			if hostEntry.IPAddress == nil {
+				return nil, fmt.Errorf("invalid ip address: %s", tok)
+			}
+
+		// fqdn
+		case 1:
+			hostEntry.DomainName = tok
+
+		// alias
+		case 2:
+			hostEntry.Alias = tok
+
+		// Can only be a comment and shouldn't reach here
+		case 3:
+			return nil, fmt.Errorf("expecting last entry to be a comment starting with a '#' character - got '%s'", tokens[n:])
+		}
+	}
+
+	return hostEntry, hostEntry.Validate()
 }
 
 type HostsFileCtl struct {
 	HostsFile string
+	Entries []HostEntry
 }
 
-func parseLine(line []byte) ([]string, error) {
+func tokenize(line []byte) ([]string, error) {
 
-	log.Printf("--- %s ---", line)
 	rawLine := line
 	tmpBuf := make([]byte, len(rawLine))
 	copy(tmpBuf, rawLine)
@@ -33,12 +137,12 @@ func parseLine(line []byte) ([]string, error) {
 	tmpBuf = bytes.TrimRight(bytes.TrimLeft(tmpBuf, " "), "\t")
 
 	tokens := make([]string, 0)
-	var entry int
 	for {
 
-		log.Printf("Checking line: %s - %t", tmpBuf, false)
 		i, words, err := bufio.ScanWords(tmpBuf, false)
-		log.Printf("HERE %d: %d - '%s' - %v", entry, i, words, err)
+		if err != nil {
+			return nil, err
+		}
 
 		if i == 0 {
 			tokens = append(tokens, fmt.Sprintf("%s", tmpBuf))
@@ -50,13 +154,9 @@ func parseLine(line []byte) ([]string, error) {
 			break
 		}
 
-		entry++
 		tokens = append(tokens, fmt.Sprintf("%s", words))
 		tmpBuf = bytes.TrimLeft(bytes.TrimLeft(tmpBuf[i:], " "), "\t")
-
-		if entry > 10 {
-			return nil, fmt.Errorf("tokens exceeded")
-		}
+		tmpBuf = bytes.TrimLeft(bytes.TrimLeft(tmpBuf, " "), "\t")
 	}
 
 	return tokens, nil
@@ -64,20 +164,22 @@ func parseLine(line []byte) ([]string, error) {
 
 func (hfc *HostsFileCtl) Parse(hostFilePath string) error {
 
+	hfc.HostsFile = hostFilePath
+	if hfc.Entries == nil {
+		hfc.Entries = make([]HostEntry, 0)
+	}
+
 	f, err := os.Open(hostFilePath)
 	if err != nil {
 		return err
 	}
 	defer f.Close()
 
-	hfc.HostsFile = hostFilePath
 	rdr := bufio.NewReader(f)
-
 	var lineNumber int
 	for {
 
 		line, prefix, err := rdr.ReadLine()
-		log.Printf(	"%d - %s", lineNumber, line)
 
 		if prefix {
 			return fmt.Errorf("line is too long: %d", lineNumber)
@@ -89,21 +191,43 @@ func (hfc *HostsFileCtl) Parse(hostFilePath string) error {
 			}
 		}
 
+		// Skip newlines
 		if len(line) <= 0 {
 			lineNumber++
 			continue
 		}
 
-		tokens, err := parseLine(line)
+		entry, err := NewHostEntry(line)
 		if err != nil {
+			return fmt.Errorf("invalid host entry on line %d - %s", lineNumber, err)
+		}
+
+		lineNumber++
+		hfc.Entries = append(hfc.Entries, *entry)
+	}
+}
+
+func (hfc *HostsFileCtl) Write(writer io.Writer) error {
+
+	if hfc.Entries == nil || len(hfc.Entries) <= 0 {
+		return nil
+	}
+
+	for n, entry := range hfc.Entries {
+
+		var prefix string
+
+		// Only append prefix if the previous line was a comment only
+		if n != 0 {
+			prevEntry := hfc.Entries[n-1]
+			if len(entry.Comment) > 0 && entry.IPAddress == nil && prevEntry.IPAddress != nil {
+				prefix = fmt.Sprintf("\r\n")
+			}
+		}
+
+		if err := entry.Write(writer, prefix); err != nil {
 			return err
 		}
-
-		log.Printf("%d - %s", len(tokens), tokens)
-		lineNumber++
-
-		if lineNumber > 20 {
-			return nil
-		}
 	}
+	return nil
 }
--- hosts_test.go
+++ hosts_test.go
@@ -1,13 +1,43 @@
 package go_hostctl
 
-import "testing"
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+)
 
-func TestHostsFileCtl_ParseTabs(t *testing.T) {
+func TestHostsFileCtl_Parse(t *testing.T) {
 
-	tabHosts := new(HostsFileCtl)
-	if err := tabHosts.Parse( "testdata/etc/hosts/tab_hosts"); err != nil {
+	hosts := new(HostsFileCtl)
+	if err := hosts.Parse( "testdata/etc/hosts/mixed_hosts"); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestHostsFileCtl_Write(t *testing.T) {
+
+	f, err := ioutil.TempFile(os.TempDir(), "hostfilectl")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		defer os.Remove(f.Name())
+
+		out, err := ioutil.ReadFile(f.Name())
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		t.Logf("\r\n%s", out)
+	}()
+	defer f.Close()
+
+	hosts := new(HostsFileCtl)
+	if err := hosts.Parse( "testdata/etc/hosts/mixed_hosts"); err != nil {
 		t.Fatal(err)
 	}
 
-
+	if err := hosts.Write(f); err != nil {
+		t.Fatal(err)
+	}
 }
